cf7f3ec1a5249342753abbc41a6cdee1c533895d
lacebx <arsenemanzi@pm.me>
2025-02-26 00:52:08 -0500

    ds

--- app/config/passport.js
+++ app/config/passport.js
@@ -46,3 +46,6 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
+
+console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("Google Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
